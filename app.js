1d710406876c9dc6101726d9d2c7f2a2b4d1fd68
Marcin Gromek <gromson101@gmail.com>
2023-04-17 13:16:36 +0200

    feature: sticky nav, toggle mobile nav

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Working');
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleButton = document.querySelector('.navbar__menu--toggle');
+  if (!toggleButton) {
+    return;
+  }
+  toggleButton.addEventListener('click', toggleMenu, false);
+});
+
+function toggleMenu() {
+  const menu = document.querySelector('.navbar__menu');
+  menu.classList.toggle('navbar__menu--active');
+}
